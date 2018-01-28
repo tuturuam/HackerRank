b603ad89173f2fa46d983e3dc9074fef03088d07
tuturuam <aizadmoh@gmail.com>
2018-01-27 19:38:27 -0500

    On branch master
     Initial commit
    
     Changes to be committed:
    	new file:   Hello/a.out
    	new file:   Hello/hello.cpp
    	new file:   Hello/hello2.cpp
    	new file:   Hello/hello3.cpp

---
+++ Hello/hello3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x;
+	for (x = 0; x <= 5; x++) {
+		cout<< "Hello "<< x << "\n";
+		if (x == 3) cout<< "Hi There! \n";
+	}
+	return 0;
+}
